38a640b0495a574db9217e547ef280f7b78ed50e
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2024-09-20 10:10:39 +0800

    Add files via upload

---
+++ Chapter 3/ FirstAddressExcercise.java
@@ -0,0 +1,51 @@
+class FirstAddressExercise {
+   
+   
+   public static void main(String[] args) {
+   
+   displayAddress();
+   displayStateAddress();
+   displayZipcodeAddress(); 
+   displayCountry();
+   phoneBrand();  
+   companyName();   
+   }
+   
+   
+   public static void displayAddress(){
+   
+   System.out.println("Street: 2566 Hc 1");
+   
+   }
+   
+   public static void displayStateAddress() {
+   
+   System.out.println("State: Alaska (AK)");
+   
+   }
+   
+   public static void displayZipcodeAddress(){
+   
+   System.out.println("Zipcode: 99588");
+   
+   }
+   
+   public static void displayCountry() {
+   
+   System.out.println("Country: USA");
+   
+   }
+   
+   public static void phoneBrand() {
+   
+   System.out.println("Phone brand: Iphone");
+   
+   }
+   
+   public static void companyName(){
+   
+   System.out.println("Company name: Apple. Inc");
+   
+   }   
+
+}
