7be893aa93bd5a102f354ea0aee739c8f77d2aaf
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2024-09-27 18:34:44 +0700

    Add files via upload

---
+++ Chapter 4/Circle.java
@@ -0,0 +1,32 @@
+
+public class Circle {
+    public static double area, radius, diameter;
+
+    public Circle() {
+        radius = 1;
+        diameter = 2 * radius ;
+        area = Math.PI * radius * radius; 
+    }
+
+
+    public void setRadius(double rad) {
+        radius = rad;
+        diameter = 2 * radius;
+        area = Math.PI * radius * radius;
+    }
+
+    public static double getRadius() {
+        return radius;
+    }
+
+    public static double getDiameter() {
+        return diameter;
+    }
+
+    public static double getArea() {
+        return area;
+    }
+
+
+    
+}
