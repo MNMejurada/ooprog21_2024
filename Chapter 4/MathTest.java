e4e2416aecd7ff7b13526f26bd044ead7504d01e
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2024-10-04 07:07:38 +0700

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,13 @@
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println("SQRT: " + Math.sqrt(37));
+        System.out.println("SIN: " + Math.sin(Math.toRadians(270))); // Converting degrees to radians
+        System.out.println("COS: " + Math.cos(Math.toRadians(270))); // Converting degrees to radians
+        System.out.println("FLOOR: " + Math.floor(22.8));
+        System.out.println("CEIL: " + Math.ceil(22.2));
+        System.out.println("ROUND: " + Math.round(22.5));
+        System.out.println("MAX: " + Math.max(71, 68));
+        System.out.println("MIN: " + Math.min(71, 68));
+        System.out.println("RANDOM: " + (int)(Math.random() * 100)); // Random integer between 0 and 99
+    }
+}
