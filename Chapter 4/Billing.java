f7358bceba8f7acaf8d325d38367b4f2800dca34
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2024-10-04 07:37:28 +0700

    Billing.java

--- Chapter 4/Billing.java
+++ Chapter 4/Billing.java
@@ -1,21 +1,22 @@
 class Billing {
-    public double computeBill(double price) {
+   public static double computeBill(double price) {
         double total = price * 1.08;         
         System.out.println("Total bill: $" + total);
         return total;
     }
 
-    public double computeBill(double price, int quantity) {
-        double total = (price * quantity) + (price * 1.08);         
+    public static double computeBill(double price, int quantity) {
+        double total = price * quantity * 1.08;         
         System.out.println("Total bill: $" + total);
         return total;
     }
 
-    public double computeBill(double price, int quantity, int couponDiscount) {
-        double total = (price * quantity) + (price * quantity * couponDiscount / 100) * 1.08;         
+    public static double computeBill(double price, int quantity, int discount) {
+        double total = (price * quantity * 1.08) - discount;         
         System.out.println("Total bill: $" + total);
         return total;
     } 
+
     
     public static void main(String[] args) {
         Billing billing = new Billing();
