20986cdf33c99b12b6c76c41bc5fc0116fb013c9
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2025-03-06 09:10:35 +0800

    Add files via upload

---
+++ Chapter 1/ProjectAppsDev1/ProjectAppsDev1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProjectAppsDev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(587, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(499, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "........";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(598, 366);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please Enter a # between 1-12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 790);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Activity 2A";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Chapter 1/ProjectAppsDev1/ProjectAppsDev1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectAppsDev1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text.Trim(); 
+            int ruleNumber;
+
+            // Validate input
+            if (int.TryParse(input, out ruleNumber) && ruleNumber >= 1 && ruleNumber <= 12)
+            {
+                label1.Text = GetRuleDescription(ruleNumber);
+            }
+            else
+            {
+                label1.Text = "Please enter a valid number between 1 and 12.";
+            }
+
+        }
+
+        private string GetRuleDescription(int number)
+        {
+            switch (number)
+            {
+                case 1: return "Rule 1 Maintain silence, proper decorum, \r" +
+                                "\nand discipline inside the laboratory. \r" +
+                                "\nMobile phones, walkmans and other \r" +
+                                "\npersonal pieces of equipment must be switched off.\r\n";
+
+                case 2: return "Rule 2Games are not allowed inside the lab. \r" +
+                                "\nThis includes computer-related games, \r" +
+                                "\ncard games and other games that may disturb the operation of the lab.";
+
+                case 3: return "Rule 3 Surfing the Internet is allowed only with the permission of the instructor. \r" +
+                               "\nDownloading and installing of software are strictly prohibited.";
+
+                case 4: return "Rule 4 Getting access to other websites not related to the course \r" +
+                                "\n(especially pornographic and illicit sites) " +
+                                "is strictly prohibited.\r\n";
+
+                case 5: return "Rule 5 Deleting computer files and changing \r" +
+                               "\nthe set-up of the computer is a major offense.";
+
+                case 6: return "Rule 6 Observe computer time usage carefully. A fifteen-minute \r" +
+                                "\nallowance is given for each use. Otherwise, the unit will be given \r" +
+                                "\nto those who wish to \"sit-in\".";
+
+                case 7: return "Rule 7 Observe proper decorum while inside the laboratory. \r" +
+                                "\na. Do not get inside the lab unless the instructor is present.\r" +
+                                "\nb. All bags, knapsacks, and the likes must be deposited at the counter.\r" +
+                                "\nc. Follow the seating arrangement of your instructor.\r" +
+                                "\nd. At the end of class, all software programs must be closed.\r" +
+                                "\ne. Return all chairs to their proper places after using.\r\n";
+
+                case 8: return "Rule 8 Chewing gum, eating, drinking, smoking, and other\r" +
+                                "\nforms of vandalism are prohibited inside the lab.\r\n";
+
+                case 9: return "Rule 9 Anyone causing a continual disturbance will be asked to leave the lab. \r" +
+                                "\nActs or gestures offensive to the members of the community,\r" +
+                                "\n including public display of physical intimacy, are not tolerated.";
+
+                case 10: return "Rule 10 Persons exhibiting hostile or threatening behavior such as yelling, swearing, \r" +
+                                "\nor disregarding requests made by lab personnel will be asked to leave the lab.";
+
+                case 11: return "Rule 11 For serious offense, the lab personnel may call the \r" +
+                                "\nCivil Security Office (CSU) for assistance.";
+
+                case 12: return "Rule 12 Any technical problem or difficulty must be addressed to the \r" +
+                                "\nlaboratory supervisor, student assistant or instructor immediately.";
+
+                default: return "Invalid rule number.";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "You’re proof that even evolution makes mistakes.";
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
